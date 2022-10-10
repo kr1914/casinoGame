0b793a36d651c3a9a2b26f7381c8e085cfbf7e14
김태선 <lorelime@gimtaeseon-ui-MacBookAir.local>
2022-10-10 21:14:59 +0900

    typescript setting

---
+++ bin/sample.js
@@ -0,0 +1,3 @@
+console.log("Hellow World!!");
+console.log("Helloooooo!!");
+//# sourceMappingURL=sample.js.map
---
+++ src/Controller.ts
@@ -0,0 +1,26 @@
+import { createRequire, syncBuiltinESMExports } from "module";
+const require = createRequire(import.meta.url);
+
+
+const http = require('http');
+const websocket = require('ws');
+
+
+const server = http.createServer((req, rsp) => {
+    rsp.writeHead(200, {'Content-type':'text/html'});
+    rsp.end('Hello node.js');
+});
+
+server.listen(3030, () =>{
+    console.log("server Listening");
+});
+const wbss = new websocket.Server({server:server});
+
+wbss.on('connection', (wb) => {
+    console.log('socket connected');
+    console.log(wb);
+    wb.send("socket Service on");
+    
+});
+
+
